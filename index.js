60b71bb52e84031ac6c6057290e09692079ca1c7
prince <mrepte700@gmail.com>
2021-07-09 14:57:22 +0600

    Schema & Model

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUn
     .then(() => console.log('Database is Connected!'))
     .catch((err) => console.error(err));
 
+// schema = shape of a document
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    age : Number,
+    isMarried : Boolean,
+    gender : String,
+    sallary : Number 
+});
+
+const User = mongoose.model('User', userSchema);
